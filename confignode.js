ba65db024533b968301cf85736bdfee83bac07f6
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-05-26 13:58:37 -0300

    Create confignode.js

---
+++ confignode.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+
+    paths:{
+    index: { dir: "index.html", mime: { 'Content-Type': 'text/html' } },
+    raiz: { dir: "/", mime: { 'Content-Type': 'text/html' } },
+    css: { dir: "/css/", mime: { 'Content-Type': 'text/css' } },
+    img: { dir: "/images/", mime: { 'Content-Type': 'image/jpg' } },
+    views: { dir: "/views/", mime: { 'Content-Type': 'text/html' } },
+    script: { dir: "/css/", mime: { 'Content-Type': 'text/javascript' } },
+}
+
+
+}
