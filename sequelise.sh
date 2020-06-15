f18c8cf51057b89b5abaa89930cb2b95598ccfa6
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2020-06-15 14:38:14 -0300

    Create sequelise.sh

---
+++ sequelise.sh
@@ -0,0 +1,5 @@
+$ npm install --save pg pg-hstore # Postgres
+$ npm install --save mysql2
+$ npm install --save mariadb
+$ npm install --save sqlite3
+$ npm install --save tedious # Microsoft SQL Server
