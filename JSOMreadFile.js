5dccd8d90a7ff0ebfab1fc75f091fb290911f31c
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:29:15 -0300

    Create JSOMreadFile.js

---
+++ JSOMreadFile.js
@@ -0,0 +1,30 @@
+function readFile(resultpanel) {
+    var clientContext;
+    var oWebsite;
+    var fileUrl;
+
+    clientContext = new SP.ClientContext.get_current();
+    oWebsite = clientContext.get_web();
+
+    clientContext.load(oWebsite);
+    clientContext.executeQueryAsync(function () {
+        fileUrl = oWebsite.get_serverRelativeUrl() +
+            "/Lists/Shared Documents/TextFile1.txt";
+        $.ajax({
+            url: fileUrl,
+            type: "GET"
+        })
+            .done(Function.createDelegate(this, successHandler))
+            .error(Function.createDelegate(this, errorHandler));
+    }, errorHandler);
+
+    function successHandler(data) {
+        resultpanel.innerHTML =
+            "The content of file \"TextFile1.txt\": " + data
+    }
+
+    function errorHandler() {
+        resultpanel.innerHTML =
+            "Request failed: " + arguments[2];
+    }
+}
