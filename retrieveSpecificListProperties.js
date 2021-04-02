4c91d0724da050d037138efd7482fe1db29284c3
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:23:11 -0300

    Create retrieveSpecificListProperties.js

---
+++ retrieveSpecificListProperties.js
@@ -0,0 +1,28 @@
+function retrieveSpecificListProperties(siteUrl) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oWebsite = clientContext.get_web();
+    this.collList = oWebsite.get_lists();
+
+    clientContext.load(collList, 'Include(Title, Id)');
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded), 
+        Function.createDelegate(this, this.onQueryFailed)
+    );
+}
+
+function onQuerySucceeded() {
+    var listInfo = '';
+    var listEnumerator = collList.getEnumerator();
+
+    while (listEnumerator.moveNext()) {
+        var oList = listEnumerator.get_current();
+        listInfo += 'Title: ' + oList.get_title() + 
+            ' ID: ' + oList.get_id().toString() + '\n';
+    }
+    alert(listInfo);
+}
+
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() + 
+        '\n' + args.get_stackTrace());
+}
