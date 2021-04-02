d3654ea3a1f25134adae5be39c32f37136d0047b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:29:55 -0300

    Create JSOMdeleteFile.cs

---
+++ JSOMdeleteFile.cs
@@ -0,0 +1,32 @@
+function deleteFile(resultpanel) {
+    var clientContext;
+    var oWebsite;
+    var fileUrl;
+
+    clientContext = new SP.ClientContext.get_current();
+    oWebsite = clientContext.get_web();
+
+    clientContext.load(oWebsite);
+    clientContext.executeQueryAsync(function () {
+        fileUrl = oWebsite.get_serverRelativeUrl() +
+            "/Lists/Shared Documents/TextFile1.txt";
+        this.fileToDelete = oWebsite.getFileByServerRelativeUrl(fileUrl);
+        this.fileToDelete.deleteObject();
+
+        clientContext.executeQueryAsync(
+            Function.createDelegate(this, successHandler),
+            Function.createDelegate(this, errorHandler)
+        );
+    }, errorHandler);
+
+    function successHandler() {
+        resultpanel.innerHTML =
+            "Go to the " +
+            "<a href='../Lists/Shared Documents'>document library</a> " +
+            "to confirm that the \"TextFile1.txt\" file has been deleted.";
+    }
+
+    function errorHandler() {
+        resultpanel.innerHTML = "Request failed: " + arguments[1].get_message();
+    }
+}
