79263eefa509a2f4454f47a20a8f92c9a43b8bf5
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:22:07 -0300

    Create updateWebSite.js

---
+++ updateWebSite.js
@@ -0,0 +1,25 @@
+function updateWebSite(siteUrl) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    this.oWebsite = clientContext.get_web();
+
+    this.oWebsite.set_title('Updated Web Site');
+    this.oWebsite.set_description('This is an updated website.');
+    this.oWebsite.update();
+
+    clientContext.load(this.oWebsite, 'Title', 'Description');
+
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded), 
+        Function.createDelegate(this, this.onQueryFailed)
+    );
+}
+
+function onQuerySucceeded(sender, args) {
+    alert('Title: ' + this.oWebsite.get_title() + 
+        ' Description: ' + this.oWebsite.get_description());
+}
+    
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() + 
+        '\n' + args.get_stackTrace());
+}
