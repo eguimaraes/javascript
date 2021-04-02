51d07d30b32afb72ce168a03e3b9e246d5f15d90
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:24:16 -0300

    Create retrieveAllListsAllFields.js

---
+++ retrieveAllListsAllFields.js
@@ -0,0 +1,40 @@
+function retrieveAllListsAllFields(siteUrl) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oWebsite = clientContext.get_web();
+    var collList = oWebsite.get_lists();
+
+    this.listInfoArray = clientContext.loadQuery(collList, 
+        'Include(Title,Fields.Include(Title,InternalName))');
+
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded), 
+        Function.createDelegate(this, this._onQueryFailed)
+    );
+}
+
+function onQuerySucceeded() {
+    var listInfo = '';
+
+    for (var i = 0; i < this.listInfoArray.length; i++) {
+        var oList = this.listInfoArray[i];
+        var collField = oList.get_fields();
+        var fieldEnumerator = collField.getEnumerator();
+            
+        while (fieldEnumerator.moveNext()) {
+            var oField = fieldEnumerator.get_current();
+            var regEx = new RegExp('name', 'ig');
+            
+            if (regEx.test(oField.get_internalName())) {
+                listInfo += '\nList: ' + oList.get_title() + 
+                    '\n\tField Title: ' + oField.get_title() + 
+                    '\n\tField Name: ' + oField.get_internalName();
+            }
+        }
+    }
+    alert(listInfo);
+}
+
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() + 
+        '\n' + args.get_stackTrace());
+}
