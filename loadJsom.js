4a317aa2dd8ae034bb8e109a2c3a5bc45efae913
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:19:39 -0300

    Create loadJsom.js

---
+++ loadJsom.js
@@ -0,0 +1,56 @@
+https://docs.microsoft.com/en-us/sharepoint/dev/sp-add-ins/complete-basic-operations-using-javascript-library-code-in-sharepoint
+
+<script 
+    src="//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js" 
+    type="text/javascript">
+</script>
+<script
+    type="text/javascript"
+    src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.7.2.min.js">
+</script>
+<script type="text/javascript">
+    var hostweburl;
+
+    // Load the required SharePoint libraries.
+    $(document).ready(function () {
+
+        // Get the URI decoded URLs.
+        hostweburl =
+            decodeURIComponent(
+                getQueryStringParameter("SPHostUrl")
+        );
+
+        // The js files are in a URL in the form:
+        // web_url/_layouts/15/resource_file
+        var scriptbase = hostweburl + "/_layouts/15/";
+
+        // Load the js files and continue to
+        // the execOperation function.
+        $.getScript(scriptbase + "SP.Runtime.js",
+            function () {
+                $.getScript(scriptbase + "SP.js", execOperation);
+            }
+        );
+    });
+
+    // Function to execute basic operations.
+    function execOperation() {
+
+        // Continue your program flow here.
+
+    }
+
+    // Function to retrieve a query string value.
+    // For production purposes you may want to use
+    // a library to handle the query string.
+    function getQueryStringParameter(paramToRetrieve) {
+        var params =
+            document.URL.split("?")[1].split("&amp;");
+        var strParams = "";
+        for (var i = 0; i < params.length; i = i + 1) {
+            var singleParam = params[i].split("=");
+            if (singleParam[0] == paramToRetrieve)
+                return singleParam[1];
+        }
+    }
+</script>
