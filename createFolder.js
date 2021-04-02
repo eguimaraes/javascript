a3dcf751472e2d5b1a874575303af4e13ad7473b
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:25:34 -0300

    Create createFolder.js

---
+++ createFolder.js
@@ -0,0 +1,34 @@
+function createFolder(resultpanel) {
+    var clientContext;
+    var oWebsite;
+    var oList;
+    var itemCreateInfo;
+
+    clientContext = new SP.ClientContext.get_current();
+    oWebsite = clientContext.get_web();
+    oList = oWebsite.get_lists().getByTitle("Shared Documents");
+
+    itemCreateInfo = new SP.ListItemCreationInformation();
+    itemCreateInfo.set_underlyingObjectType(SP.FileSystemObjectType.folder);
+    itemCreateInfo.set_leafName("My new folder!");
+    this.oListItem = oList.addItem(itemCreateInfo);
+    this.oListItem.set_item("Title", "My new folder!");
+    this.oListItem.update();
+
+    clientContext.load(this.oListItem);
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, successHandler),
+        Function.createDelegate(this, errorHandler)
+    );
+
+    function successHandler() {
+        resultpanel.innerHTML = "Go to the " +
+            "<a href='../Lists/Shared Documents'>document library</a> " +
+            "to see your new folder.";
+    }
+
+    function errorHandler() {
+        resultpanel.innerHTML =
+            "Request failed: " + arguments[1].get_message();
+    }
+}
