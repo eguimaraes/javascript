7a7cd983b7d299bbc10531a6f8aa34b1f74090a2
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:25:13 -0300

    Create deleteList.js

---
+++ deleteList.js
@@ -0,0 +1,23 @@
+function deleteList(siteUrl) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oWebsite = clientContext.get_web();
+    this.listTitle = 'My Announcements List';
+
+    this.oList = oWebsite.get_lists().getByTitle(listTitle);
+    oList.deleteObject();
+
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded), 
+        Function.createDelegate(this, this.onQueryFailed)
+    );
+}
+
+function onQuerySucceeded() {
+    var result = listTitle + ' deleted.';
+    alert(result);
+}
+
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() + 
+        '\n' + args.get_stackTrace());
+}
