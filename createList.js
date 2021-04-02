f7b247ea6ec147cb4dd73b873c6e1765607de9cc
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:24:33 -0300

    Create createList.js

---
+++ createList.js
@@ -0,0 +1,26 @@
+function createList(siteUrl) {
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oWebsite = clientContext.get_web();
+    
+    var listCreationInfo = new SP.ListCreationInformation();
+    listCreationInfo.set_title('My Announcements List');
+    listCreationInfo.set_templateType(SP.ListTemplateType.announcements);
+
+    this.oList = oWebsite.get_lists().add(listCreationInfo);
+
+    clientContext.load(oList);
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded), 
+        Function.createDelegate(this, this.onQueryFailed)
+    );
+}
+
+function onQuerySucceeded() {
+    var result = oList.get_title() + ' created.';
+    alert(result);
+}
+
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() + 
+        '\n' + args.get_stackTrace());
+}
