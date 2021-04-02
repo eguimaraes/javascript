d189f4a4c15e1168abd314468580edc0041cc23a
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:33:12 -0300

    Create JSOMexecCrossDomainRequest.js

---
+++ JSOMexecCrossDomainRequest.js
@@ -0,0 +1,36 @@
+function execCrossDomainRequest(appweburl, hostweburl) {
+    // context: The ClientContext object provides access to
+    //      the web and lists objects.
+    // factory: Initialize the factory object with the
+    //      add-in web URL.
+    var context;
+    var factory;
+    var appContextSite;
+
+    context = new SP.ClientContext(appweburl);
+    factory = new SP.ProxyWebRequestExecutorFactory(appweburl);
+    context.set_webRequestExecutorFactory(factory);
+    appContextSite = new SP.AppContextSite(context, hostweburl);
+
+    this.web = appContextSite.get_web();
+    context.load(this.web);
+
+    // Execute the query with all the previous 
+    //  options and parameters.
+    context.executeQueryAsync(
+        Function.createDelegate(this, successHandler), 
+        Function.createDelegate(this, errorHandler)
+    );
+
+    // Function to handle the success event.
+    // Prints the host web's title to the page.
+    function successHandler() {
+        alert(this.web.get_title());
+    }
+
+    // Function to handle the error event.
+    // Prints the error message to the page.
+    function errorHandler(data, errorCode, errorMessage) {
+        alert("Could not complete cross-domain call: " + errorMessage);
+    }
+}
