05decc8d103628a97e7c7c4e2acac5f42ce79004
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-02 15:32:05 -0300

    Create JSOMdeleteListItem.js

---
+++ JSOMdeleteListItem.js
@@ -0,0 +1,21 @@
+function deleteListItem(siteUrl) {
+    this.itemId = 2;
+    var clientContext = new SP.ClientContext(siteUrl);
+    var oList = clientContext.get_web().get_lists().getByTitle('Announcements');
+    this.oListItem = oList.getItemById(itemId);
+    oListItem.deleteObject();
+
+    clientContext.executeQueryAsync(
+        Function.createDelegate(this, this.onQuerySucceeded), 
+        Function.createDelegate(this, this.onQueryFailed)
+    );
+}
+
+function onQuerySucceeded() {
+    alert('Item deleted: ' + itemId);
+}
+
+function onQueryFailed(sender, args) {
+    alert('Request failed. ' + args.get_message() + 
+        '\n' + args.get_stackTrace());
+}
