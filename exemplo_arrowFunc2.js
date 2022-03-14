f366111849ca40018348d55ec4c1c0e8ccb7fe08
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2022-03-14 14:55:21 -0300

    Create exemplo_arrowFunc2.js

---
+++ exemplo_arrowFunc2.js
@@ -0,0 +1,29 @@
+function callMe(name) { 
+    console.log(name);
+}
+
+
+const callMe = function(name) { 
+    console.log(name);
+}
+
+
+const callMe = (name) => { 
+    console.log(name);
+}
+
+
+const callMe = () => { 
+    console.log('Max!');
+}
+
+const callMe = name => { 
+    console.log(name);
+}
+
+const returnMe = name => name
+
+
+const returnMe = name => { 
+    return name;
+}
